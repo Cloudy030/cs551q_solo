71d6893a9b126c0e23ff8eb5ea6423022be4c7d5
POK NGA HO <p.ho.22@abdn.ac.uk>
2023-04-08 18:25:57 +0000

    2 test game cases added

--- games/tests/test_models.py
+++ games/tests/test_models.py
@@ -52,18 +52,29 @@ class GameModelTestCase(TestCase):
   def setUpTestData(cls):
     Platform.objects.create(
       platform_name='Wii',
-      url='https://tse1.mm.bing.net/th?id=OIP.CtiRaE9ucOFqtZGzjkR0BQHaHa&pid=Api'
+      url='https://test_wii_url'
       )
-    platform1=Platform.objects.get()
+    platform1=Platform.objects.get(platform_name='Wii')
+    Platform.objects.create(
+      platform_name='2600',
+      url='https://test_2600_url'
+      )
+    platform2=Platform.objects.get(platform_name='2600')
 
     Year.objects.create(year_no='2023')
-    year1=Year.objects.get()
+    year1=Year.objects.get(year_no='2023')
+    Year.objects.create(year_no='2022')
+    year2=Year.objects.get(year_no='2022')
 
     Genre.objects.create(genre_name='Racing')
-    genre1=Genre.objects.get()
+    genre1=Genre.objects.get(genre_name='Racing')
+    Genre.objects.create(genre_name='RPG')
+    genre2=Genre.objects.get(genre_name='RPG')
 
     Publisher.objects.create(publisher_name='20th Century Fox Video Games')
-    publisher1=Publisher.objects.get()
+    publisher1=Publisher.objects.get(publisher_name='20th Century Fox Video Games')
+    Publisher.objects.create(publisher_name='Zoo Games')
+    publisher2=Publisher.objects.get(publisher_name='Zoo Games')
 
     Game.objects.create(
       rank=111,
@@ -78,22 +89,22 @@ class GameModelTestCase(TestCase):
       other_sales=3.77,
       global_sales=21.87,
     )
-    # Game.objects.create(
-    #   rank=112,
-    #   name='Pokemon2',
-    #   platform='PC',
-    #   year='2022',
-    #   genre='Racing',
-    #   publisher='Zoo Games',
-    #   na_sales=1.3,
-    #   eu_sales=5.55,
-    #   jp_sales=7.8,
-    #   other_sales=3.78,
-    #   global_sales=18.43,
-    # )
+    Game.objects.create(
+      rank=112,
+      name='Pokemon2',
+      platform=platform2,
+      year=year2,
+      genre=genre2,
+      publisher=publisher2,
+      na_sales=1.3,
+      eu_sales=5.55,
+      jp_sales=7.8,
+      other_sales=3.78,
+      global_sales=18.43,
+    )
 
   def test_game(self):
     game=Game.objects.get(id=1)
     self.assertEqual(game.rank,111)
     games=Game.objects.all()
-    self.assertEqual(games.count(),1)
+    self.assertEqual(games.count(),2)
