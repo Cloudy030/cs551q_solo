69a53483de9b1adbd9794fd5ca357206492ac26b
POK NGA HO <p.ho.22@abdn.ac.uk>
2023-04-15 03:07:05 +0000

    filter function work

--- games/views.py
+++ games/views.py
@@ -5,6 +5,7 @@ from .models import Year, Genre, Platform, Publisher, Game
 # Create your views here.
 def index(request):
     games=Game.objects.all()
+    # for dropdown search bar
     platforms=Platform.objects.all()
     years=Year.objects.all()
     genres=Genre.objects.all()
@@ -19,10 +20,66 @@ def gamefilter(request):
     games=Game.objects.all()
     # for drop down search bar
     platforms=Platform.objects.all()
+    years=Year.objects.all()
+    genres=Genre.objects.all()
+    publishers=Publisher.objects.all()
+    # filter function
     pl=request.POST.get('plfilter')
     print('platform: ',pl,request)
-    # if pl==None:
-    return render(request, 'games/game.html', {'games':games})
+    y=request.POST.get('yfilter')
+    print('year: ',y,request)
+    g=request.POST.get('gfilter')
+    print('genre: ',g,request)
+    pu=request.POST.get('pufilter')
+    print('publisher: ',pu,request)
+
+    # 0 filter
+    if pl==None and y==None and g==None and pu==None:
+        filgame=games
+        # show all data rows if no filter requirements are given
+
+    # 1 filter
+    elif y==None and g==None and pu==None:
+        filgame=Game.objects.filter(platform__platform_name=pl)
+            # https://stackoverflow.com/questions/67988454/field-id-expected-a-number-but-got
+    elif pl==None and g==None and pu==None:
+        filgame=Game.objects.filter(year__year_no=y)
+    elif pl==None and y==None and pu==None:
+        filgame=Game.objects.filter(genre__genre_name=g)
+    elif pl==None and y==None and g==None:
+        filgame=Game.objects.filter(publisher__publisher_name=pu)
+
+    # 2 filter
+    elif g==None and pu==None:
+        filgame=Game.objects.filter(platform__platform_name=pl).filter(year__year_no=y)
+    elif y==None and pu==None:
+        filgame=Game.objects.filter(platform__platform_name=pl).filter(genre__genre_name=g)
+    elif y==None and g==None:
+        filgame=Game.objects.filter(platform__platform_name=pl).filter(publisher__publisher_name=pu)
+    
+    elif pl==None and pu==None:
+        filgame=Game.objects.filter(year__year_no=y).filter(genre__genre_name=g)
+    elif pl==None and g==None:
+        filgame=Game.objects.filter(year__year_no=y).filter(publisher__publisher_name=pu)
+
+    elif pl==None and y==None:
+        filgame=Game.objects.filter(genre__genre_name=g).filter(publisher__publisher_name=pu)
+
+    # 3 filter
+    elif pl==None:
+        filgame=Game.objects.filter(year__year_no=y).filter(genre__genre_name=g).filter(publisher__publisher_name=pu)
+    elif y==None:
+        filgame=Game.objects.filter(platform__platform_name=pl).filter(genre__genre_name=g).filter(publisher__publisher_name=pu)
+    elif g==None:
+        filgame=Game.objects.filter(platform__platform_name=pl).filter(year__year_no=y).filter(publisher__publisher_name=pu)
+    elif pu==None:
+        filgame=Game.objects.filter(platform__platform_name=pl).filter(year__year_no=y).filter(genre__genre_name=g)
+    
+    # 4 filter
+    else:
+        filgame=Game.objects.filter(platform__platform_name=pl).filter(year__year_no=y).filter(genre__genre_name=g).filter(publisher__publisher_name=pu)
+    
+    return render(request, 'games/game_filter.html', {'filgame':filgame, 'games':games, 'platforms':platforms, 'years':years, 'genres':genres, 'publishers':publishers})
 
 def year(request):
     years=Year.objects.all()
