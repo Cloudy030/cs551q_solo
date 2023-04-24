0ffae0a64da921f69c0b208042d8610230a6f2d4
POK NGA HO <p.ho.22@abdn.ac.uk>
2023-04-24 12:45:58 +0000

    add staff and general account

--- games/admin.py
+++ games/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Year, Genre, Platform, Publisher, Game
+from .models import Year, Genre, Platform, Publisher, Game,  Cart, Customer, LineItem, Order
 
 # Register your models here.
 
@@ -7,3 +7,11 @@ class GameAdmin(admin.ModelAdmin):
   list_display=("rank", "name", "platform", "year", "genre", "publisher", "na_sales", "eu_sales", "jp_sales", "other_sales", "global_sales",)
 
 admin.site.register(Game, GameAdmin)
+admin.site.register(Genre)
+admin.site.register(Year)
+admin.site.register(Platform)
+admin.site.register(Publisher)
+admin.site.register(Cart)
+admin.site.register(Customer)
+admin.site.register(LineItem)
+admin.site.register(Order)
